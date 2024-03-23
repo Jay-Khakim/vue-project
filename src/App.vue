64e7aaef616e910263cace2aa689e9ec81bd4643
Jay Khakim <mgmediajay@gmail.com>
2024-03-23 21:55:13 +0900

    Working on site navigation!

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-	<div>
-		<RoaterView />
+	<div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+		<SiteNavigation />
+		<RouterView />
 	</div>
 </template>
 
 <script setup>
-	IMPORT {RoaterView} from "vue-router"
+	import { RouterView } from "vue-router";
+	import SiteNavigation from "./components/SiteNavigation.vue";
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.page-enter-active {
+  transition: 600ms ease all;
+}
+
+.page-enter-from {
+  opacity: 0;
+}
+</style>
