30055d8309037699a96e4957bd0d0138d0ed5718
Jay Khakim <mgmediajay@gmail.com>
2024-03-28 09:02:37 +0900

    handling async data

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,10 +1,12 @@
 <template>
-    <div>
-
-    </div>
+	<div>
+		<Suspense>
+			<AsyncCityView />
+			<template #fallback><p>Loading ...</p> </template>
+		</Suspense>
+	</div>
 </template>
 
 <script setup>
-
+	import AsyncCityView from "../components/AsyncCityView.vue";
 </script>
-
