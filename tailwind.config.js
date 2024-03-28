83c24c1abfb8def8d3f4df3f44f9edbab86ca7a9
Jay Khakim <mgmediajay@gmail.com>
2024-03-28 10:30:48 +0900

    city view layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,37 @@
 module.exports = {
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
-	  extend: {
-		colors: {
-		  "weather-primary": "#00668A",
-		  "weather-secondary": "#004E71",
+		extend: {
+			colors: {
+				"weather-primary": "#00668A",
+				"weather-secondary": "#004E71",
+			},
 		},
-	  },
-	  fontFamily: {
-		Roboto: ["Roboto, sans-serif"],
-	  },
-	  container: {
-		padding: "2rem",
-		center: true,
-	  },
-	  screens: {
-		sm: "640px",
-		md: "768px",
-	  },
+		fontFamily: {
+			Roboto: ["Roboto, sans-serif"],
+		},
+		container: {
+			padding: "2rem",
+			center: true,
+		},
+		screens: {
+			sm: "640px",
+			md: "768px",
+		},
 	},
-	plugins: [],
-  };
+	plugins: [
+		function ({ addUtilities }) {
+			const newUtilities = {
+				".no-scrollbar::-webkit-scrollbar": {
+					display: "none",
+				},
+				"no-scrollbar": {
+					"-ms-overflow-style": "none",
+					"scrollbar-width": "none",
+				},
+			};
+
+			addUtilities(newUtilities)
+		},
+	],
+};
